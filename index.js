2926febb0c93e349a3a7609464bf3b3bc03d6d81
reinhard9921 <rengelbrecht@sts3d.co.za>
2022-02-19 15:01:53 +0200

    Server Setup

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ var nodemailer = require('nodemailer');
 
 
 const Pool = require("pg").Pool;
-require("dotenv").config();
+require(".env").config();
 const isProduction = process.env.NODE_ENV === "production";
 const connectionString = `postgresql://${process.env.PG_USER}:${process.env.PG_PASSWORD}@${process.env.PG_HOST}:${process.env.PG_PORT}/${process.env.PG_DATABASE}`;
 const pool = new Pool({
