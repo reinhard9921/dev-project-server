07d8c28b8081608bc98583808806c1a1d499eb2f
reinhard9921 <rengelbrecht@sts3d.co.za>
2022-02-22 08:55:24 +0200

    Spelling error

--- index.js
+++ index.js
@@ -16,7 +16,8 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(function (req, res, next) {
   res.header(
     "Access-Control-Allow-Origin",
-    "https://serverside-project-dev.herokuapp.com/"
+    "https://serverside-project-dev.herokuapp.com/",
+    "http://localhost:3000/"
   ); // update to match the domain you will make the request from
   res.header(
     "Access-Control-Allow-Headers",
